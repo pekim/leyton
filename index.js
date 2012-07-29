2deb9f2bf67019abd83148c8be5f1638b25edb24
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-07-29 17:22:05 +0100

    Initial commit.
    Will git clone node git repo, and git pull periodically.

---
+++ index.js
@@ -0,0 +1,21 @@
+var async = require('async');
+var charm = require('charm')(process);
+var gitRepo = require('./lib/git-repo');
+var ProgressAnimation = require('./lib/progress-animation');
+
+//charm.reset();
+
+charm.write('initialising ');
+charm.on('^C', process.exit);
+
+charm.position(function(x, y) {
+  var progressAnimation = new ProgressAnimation(charm, x, y)
+
+  progressAnimation.start();
+  gitRepo.update(function gitUpdateComplete(err) {
+    progressAnimation.stop();
+
+    console.log(err);
+    process.exit();
+  });
+});
