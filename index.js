16b4b692e5d3ed8d5418a0ae470c4d357aa6cff9
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-08-12 14:56:03 +0100

    Discovery of available (not necessarily downloaded) docs versions.

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 var async = require('async');
 var charm = require('charm')(process);
-var gitRepo = require('./lib/git-repo');
-var ProgressAnimation = require('./lib/progress-animation');
 
-charm.reset();
+var downloadedDocs = require('./lib/downloaded-docs');
 
-charm.write('initialising ');
-charm.on('^C', process.exit);
+// var ProgressAnimation = require('./lib/progress-animation');
+// var status = require('./lib/status');
 
-charm.position(function(x, y) {
-  var progressAnimation = new ProgressAnimation(charm, x, y)
+downloadedDocs.availableVersions(function(err, availableVersions) {
+  console.log(availableVersions.map(function (version) {
+    return version.name;
+  }));
 
-  progressAnimation.start();
-  gitRepo.update(function gitUpdateComplete(err) {
-    progressAnimation.stop();
+  // charm.reset();
+  // charm.on('^C', process.exit);
 
-    if (err) {
-      console.log(err);
-    }
-    process.exit();
-  });
+  process.exit();
 });
+
+// if (!status.availableDownloadsUpdatedRecently(60 * 1000)) {
+
+// }
+
+// charm.write('initialising ');
+
+// charm.position(function(x, y) {
+//   var progressAnimation = new ProgressAnimation(charm, x, y)
+
+//   progressAnimation.start();
+//   gitRepo.update(function gitUpdateComplete(err) {
+//     progressAnimation.stop();
+
+//     if (err) {
+//       console.log(err);
+//     }
+//     process.exit();
+//   });
+// });
